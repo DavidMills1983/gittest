b56761486c24ff35d9532066c04f9c56ec5b8603
David Mills <millsdg@gmail>
2024-12-12 13:49:54 +0000

    Added coach object

--- Main.java
+++ Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is no longer a test.");
 
+        Coach coach = new Coach("Ruben Amorim", "Manchester United");
+
+        System.out.println(coach);
+
     }
 }
