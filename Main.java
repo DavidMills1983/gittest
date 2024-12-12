9819a4a78ce7634b84bf32faee857e5c8f8a81a8
David Mills <millsdg@gmail>
2024-12-12 13:32:29 +0000

    This is no longer a test

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package gittest;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is a test.");
+        System.out.println("This is no longer a test.");
 
     }
 }
