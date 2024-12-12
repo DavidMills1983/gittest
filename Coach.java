87b08b88c72d7147c64f720235050807c8e11f98
David Mills <millsdg@gmail>
2024-12-12 13:44:47 +0000

    Added to coach class

--- Coach.java
+++ Coach.java
@@ -1,4 +1,35 @@
 package gittest;
 
-public class Coach {
+public class Coach implements CoachingBehaviour {
+    private String name;
+    private String club;
+
+    public Coach(String name, String club) {
+        this.name = name;
+        this.club = club;
+    }
+
+    @Override
+    public String toString() {
+        return "Coach's name=" + name + '\'' +
+                ", club='" + club + '\'' +
+                '}';
+    }
+
+    @Override
+    public void pickTeam() {
+        System.out.println("Picking the starting 11...");
+    }
+
+    @Override
+    public void chooseTactics() {
+        System.out.println("Choosing a formation...");
+
+    }
+
+    @Override
+    public void buyPlayer() {
+        System.out.println("Buying new player...");
+
+    }
 }
